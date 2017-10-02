9b9e78fdcdf7a9ae698b45f466df0df54a5d25de
ruizmartha <31838625+ruizmartha@users.noreply.github.com>
2017-10-02 01:41:37 -0500

    Add files via upload

---
+++ pro1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b=0;
+printf ("introduzca el primer numero");
+scanf ("%d",&a);
+printf  ("introduzca el segundo numero");
+scanf ("%d",&b);
+if (a<b)
+printf ("el primer numero es mayor,a");
+else 
+printf ("el segundo numero es mayor,b");
+}
---
+++ pro3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	ch a,b,c=0;
+	printf("introduzca el primer caracter");
+	scanf("%s",&a);
+	printf("introduzca el segubdo caracter");
+	scanf("%s",&b);
+	printf("introduzca el tercer caracter");
+	scanf("%s",&c);
+	if
+	
+}
